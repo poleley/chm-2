24f6aad3d56ba5f37ec0e62eb1d176e9d26d5935
Полина Половникова <polovnikovapo@mail.ru>
2021-10-30 22:32:33 +0700

    add vector norm and relative residual

---
+++ chm-2/Methods.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace chm_2
+{
+    public class Methods
+    {
+        public static double relativeResidual(double vectorNorm, double[] Ax, double[] b, int n)
+        {
+            double[] diff = new double[n];
+            for (int i = 0; i < n; i++)
+            {
+                diff[i] = b[i] - Ax[i];
+            }
+
+            return Vector.norm(diff, n) / vectorNorm;
+        }
+    }
+}
